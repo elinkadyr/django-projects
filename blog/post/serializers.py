faec5824ecbce931ff95894538e8b8d497c51259
Elina <elinkdrva@gmail.com>
2023-04-14 11:50:31 +0600

    likes

--- blog/post/serializers.py
+++ blog/post/serializers.py
@@ -5,4 +5,8 @@ class PostSerializer(ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
-        
+    
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep["likes"] = instance.likes.all().count()
+        return rep
